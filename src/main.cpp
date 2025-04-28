f87fb0b4cda595ca0af6aaf1652e53e96ceb0d12
T-Sparkes <thomassparkes@gmail.com>
2025-04-28 06:14:21 +0100

    Updated Comm Header

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
+/*
+    This file is resonponsible for forwarding packets 
+    received from the mobile robot, over the NRF24 radio, 
+    to the PC via Serial. It also forwards packets received 
+    from the PC to the mobile robot.
+*/
+
 #include <Arduino.h>
 #include <RF24.h>
 #include <RF24_config.h>
 #include <nRF24L01.h>
 #include <printf.h>
-#include "CommPacketCommon.h"
+#include "CommProtocal.hpp"
 
 #define NRF24_CE_PIN 7
 #define NRF24_CSN_PIN 8
 
-void writePacketToSerial(uint8_t* data, size_t size);
+#define PC_ADDRESS 0xA5
+#define ROBOT_ADDRESS 0xB5
+#define SERIAL_MAX_WAIT_MS 10
+#define STATUS_INTERVAL_MS 100
 
 RF24 radio(NRF24_CE_PIN, NRF24_CSN_PIN);
 uint32_t lastStatusUpdate = 0;
@@ -26,7 +36,7 @@ void setup()
 
     radio.setPALevel(RF24_PA_HIGH);
     radio.setDataRate(RF24_2MBPS);
-    radio.setRetries(1, 5);
+    radio.setRetries(1, 0);
     radio.openWritingPipe(ROBOT_ADDRESS);
     radio.openReadingPipe(1, PC_ADDRESS); 
     radio.startListening();
@@ -39,7 +49,7 @@ void loop()
     static uint32_t lastTx = 0;
 
     static EncoderDataPacket encoderRX;
-    static AnchorRangePacket rangeRX;
+    static LandmarkPacket rangeRX;
     static StatusPacket status;
 
     while (Serial.available())
@@ -54,8 +64,9 @@ void loop()
         {
             CommandPacket rxSerialPacket;
             Serial.readBytes((uint8_t*)&rxSerialPacket, sizeof(CommandPacket));
-
-            if (rxSerialPacket.header == PACKET_HEADER && rxSerialPacket.packetID == COMMAND_PACKET_ID)
+            uint16_t checksum = calculateChecksum((uint8_t*)&rxSerialPacket, sizeof(CommandPacket));
+            
+            if (rxSerialPacket.header == PACKET_HEADER && rxSerialPacket.packetID == COMMAND_PACKET_ID && rxSerialPacket.Checksum == checksum)
             {
                 radio.stopListening();
                 radioAck = radio.write((uint8_t*)&rxSerialPacket, sizeof(CommandPacket));
@@ -71,48 +82,38 @@ void loop()
     
     if (radio.available())
     {
+        uint8_t rxBuffer[32];
         uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
-        radio.read((uint8_t*)&encoderRX, sizeof(encoderRX));
-        writePacketToSerial((uint8_t*)&encoderRX, sizeof(encoderRX));
+        radio.read(rxBuffer, bytes); // read the payload into the rangeRX struct
+
+        if (rxBuffer[2] == LANDMARK_PACKET_ID)
+        {
+            memcpy(&rangeRX, rxBuffer, sizeof(LandmarkPacket)); // read the payload into the rangeRX struct
+            uint16_t checksum = calculateChecksum((uint8_t*)&rangeRX, sizeof(LandmarkPacket)); // read the payload into the rangeRX struct);
+
+            if (rangeRX.Checksum == checksum)
+            {
+                writePacketToSerial((uint8_t*)&rangeRX, sizeof(LandmarkPacket)); // read the payload into the rangeRX struct);
+            } 
+        }
+        
+        else if (rxBuffer[2] == ENCODER_PACKET_ID)
+        {
+            memcpy(&encoderRX, rxBuffer, sizeof(EncoderDataPacket)); // read the payload into the rangeRX struct
+            uint16_t checksum = calculateChecksum((uint8_t*)&encoderRX, sizeof(EncoderDataPacket)); // read the payload into the rangeRX struct);
+
+            if (encoderRX.Checksum == checksum)
+            {
+                writePacketToSerial((uint8_t*)&encoderRX, sizeof(EncoderDataPacket)); // read the payload into the rangeRX struct);
+            } 
+        }
     }
 
     if ((millis() - lastStatusUpdate) > STATUS_INTERVAL_MS)
     {
         status.connected = radioAck;
+        status.Checksum = calculateChecksum((uint8_t*)&status, sizeof(StatusPacket));
         writePacketToSerial((uint8_t*)&status, sizeof(StatusPacket));
         lastStatusUpdate = millis();
     }
-    
 }
-
-void writePacketToSerial(uint8_t* data, size_t size)
-{
-    Serial.write(data, size);
-    for (int i = size; i < 32; i++)
-    {
-        Serial.write(0x00);
-    }
-}
-
-
-    //if (true)
-    //{
-    //    EncoderTX = {PACKET_HEADER, ENCODER_PACKET_ID, 0.0, 0.0, 0.0, 0.0};
-    //    SendPacket((uint8_t*)&EncoderTX, sizeof(EncoderDataPacket));
-    //
-    //    RangeTX = {PACKET_HEADER, RANGE_PACKET_ID, 'A', 9.73};
-    //    SendPacket((uint8_t*)&RangeTX, sizeof(AnchorRangePacket));
-//
-    //    serialAck = false;
-    //    lastTx = millis();
-    //    digitalWrite(LED_BUILTIN, LOW);
-    //}
-
-    //if (serialAck == false)
-    //{
-    //    if ((millis() - lastTx) > SERIAL_MAX_WAIT_MS)
-    //    {
-    //        serialAck = true;
-    //        digitalWrite(LED_BUILTIN, HIGH);
-    //    }
-    //}
